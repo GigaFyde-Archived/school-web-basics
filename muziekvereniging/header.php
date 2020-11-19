bae54450d9c7fabb0ccb37e036ae99b91e765fe9
Laurant <laurant@gigafyde.dev>
2020-11-19 14:25:55 +0100

    Changed general layout and added files

---
+++ muziekvereniging/header.php
@@ -0,0 +1,10 @@
+<header>
+	<div class="navbar">
+		<div>
+			<span class="logo">Jeugdorkest</span>
+			<span>Home</span>
+			<span>Contact</span>
+			<span>Cursus Klarinet</span>
+		</div>
+	</div>
+</header>
---
+++ muziekvereniging/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Muziekvereniging Zevenbergen</title>
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+<?php require_once("header.php"); ?>
+
+<main>
+	<div class="body">
+		<div class="main-body">
+			<div class="item big">Hello World</div>
+			<div class="item slim">Hello World</div>
+			<div class="split-item">
+				<div class="item top">Hello World</div>
+				<div class="item bottom">Hello World</div>
+			</div>
+		</div>
+	</div>
+</main>
+
+<?php require_once("footer.php"); ?>
+
+</body>
+</html>
