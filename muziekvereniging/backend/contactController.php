c142f2d6dafcbe7634e039f4c88a4fcc94cb0774
Laurant <laurant@gigafyde.dev>
2020-11-20 09:22:57 +0100

    Forgot to upload this

---
+++ muziekvereniging/backend/contactController.php
@@ -0,0 +1,37 @@
+<?php
+
+if($_POST['action'] == "send")
+{
+	$name = $_POST['senderName'];
+	$email = $_POST['senderEmail'];
+	$message = $_POST['message'];
+	$to = "jouwemailadres@example.net";
+
+	var_dump($_POST);
+	file_put_contents("contacts.txt",
+
+"------------------------------
+Bericht van: " . $name . " (" . $email . ")
+" . $message . "
+------------------------------",
+
+	FILE_APPEND
+	);
+
+
+/*
+	//Onderstaande code mailt het bericht ook - WERKT NIET OP XAMPP
+
+	mail($to, "Contactformulier van website",
+		"------------------------------
+		Bericht van: " . $name . " (" . $email . ")
+		" . $message . "
+		------------------------------",
+	)
+*/
+
+
+	header("Location: ../contact.php?msg=Bericht+verstuurd");
+}
+
+?>
