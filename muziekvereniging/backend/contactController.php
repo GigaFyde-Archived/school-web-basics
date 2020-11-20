f5300addea981d1d33b4335bc0c11a13f3322c09
Laurant <laurant@gigafyde.dev>
2020-11-20 09:50:38 +0100

    code format

--- muziekvereniging/backend/contactController.php
+++ muziekvereniging/backend/contactController.php
@@ -1,37 +1,36 @@
 <?php
 
-if($_POST['action'] == "send")
-{
-	$name = $_POST['senderName'];
-	$email = $_POST['senderEmail'];
-	$message = $_POST['message'];
-	$to = "jouwemailadres@example.net";
+if ($_POST['action'] == "send") {
+    $name = $_POST['senderName'];
+    $email = $_POST['senderEmail'];
+    $message = $_POST['message'];
+    $to = "jouwemailadres@example.net";
 
-	var_dump($_POST);
-	file_put_contents("contacts.txt",
+    var_dump($_POST);
+    file_put_contents("contacts.txt",
 
-"------------------------------
+        "------------------------------
 Bericht van: " . $name . " (" . $email . ")
 " . $message . "
 ------------------------------",
 
-	FILE_APPEND
-	);
+        FILE_APPEND
+    );
 
 
-/*
-	//Onderstaande code mailt het bericht ook - WERKT NIET OP XAMPP
+    /*
+        //Onderstaande code mailt het bericht ook - WERKT NIET OP XAMPP
 
-	mail($to, "Contactformulier van website",
-		"------------------------------
-		Bericht van: " . $name . " (" . $email . ")
-		" . $message . "
-		------------------------------",
-	)
-*/
+        mail($to, "Contactformulier van website",
+            "------------------------------
+            Bericht van: " . $name . " (" . $email . ")
+            " . $message . "
+            ------------------------------",
+        )
+    */
 
 
-	header("Location: ../contact.php?msg=Bericht+verstuurd");
+    header("Location: ../contact.php?msg=Bericht+verstuurd");
 }
 
 ?>
