8b6d0c71f7a40275e4baf8c046aee2ad69b8493b
Laurant <laurant@gigafyde.dev>
2020-11-20 09:18:16 +0100

    Changes

--- muziekvereniging/header.php
+++ muziekvereniging/header.php
@@ -2,9 +2,7 @@
 	<div class="navbar">
 		<div>
 			<span class="logo">Jeugdorkest</span>
-			<span><a href="#">Home</a></span>
-			<span><a href="#">Contact</a></span>
-			<span><a href="#">Cursus Klarinet</a></span>
+
 		</div>
 	</div>
 </header>
--- muziekvereniging/index.php
+++ muziekvereniging/index.php
@@ -17,11 +17,28 @@
 			<div class="item"><img src="img/home-2-leerorkest.jpg" alt="leerorkest"><span>Leerorkest</span></div>
 			<div class="split-item">
 				<div class="item"><img src="img/home-3-speelgroep.jpg" alt="speelgroep"><span>Speelgroep</span></div>
-				<div class="item"><img src="img/home-4-bultseherrie.jpg" alt="bulseherrie"><span>Bultseherrie</span></div>
+				<div class="item"><img src="img/home-4-bultseherrie.jpg" alt="bulseherrie"><span>Bultseherrie</span>
+				</div>
 			</div>
 		</div>
 	</div>
 </main>
+
+<div class="info">
+	<h3>Leerorkest</h3>
+
+	<p>Het leerorkest is voor kinderen (en volwassenen?) die al les hebben op een muziekinstrument.</p>
+
+	<p>Veel kinderen hebben al eerder meegespeeld in het probeerorkest
+		en vinden het leuk om met elkaar door te gaan.</P>
+
+	<p>Vanaf ongeveer 10/11 jaar kun je meedoen met het leerorkest.
+		Maar de voorwaarde is dat je les neemt op het instrument dat je kiest.
+		Als het instrument dat je wilt bespelen bij de vereniging beschikbaar is,
+		dan kan je dat lenen.</P>
+
+	<p>Voor informatie mail naar: mooz@muziekverenigingzevenbergen.nl</p>
+</div>
 
 <?php require_once("footer.php"); ?>
 
