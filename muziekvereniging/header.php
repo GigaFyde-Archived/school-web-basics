8f90e83c62a0f8b0896d86ba5077f90b5bfe228d
Laurant <laurant@gigafyde.dev>
2020-11-20 09:29:10 +0100

    Forgot to change the name

--- muziekvereniging/header.php
+++ muziekvereniging/header.php
@@ -2,7 +2,7 @@
 	<div class="navbar">
 		<div>
 			<span class="logo"><a href="index.php">Jeugdorkest</span>
-			<span><a href="contact.php">Cursus Klarinet</a></span>
+			<span><a href="contact.php">Contact</a></span>
 		</div>
 	</div>
 </header>
